9b823255deacbb6612437ac9d2b262c4d30f6b57
chrissmol <chrisouzamol@gmail.com>
2024-03-27 20:38:29 -0300

    Adicionando classe DAO

---
+++ src/main/java/br/com/carstore/dao/CarDAO.java
@@ -0,0 +1,30 @@
+package br.com.carstore.dao;
+
+import br.com.carstore.model.Car;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CarDAO {
+    public void createCar(Car car){
+        try{
+
+            String SQL = "INSERT INTO CAR (NAME) VALUES (?)";
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
+
+            PreparedStatement preparedStatement =  connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, car.getName());
+
+            preparedStatement.execute();
+            System.out.println("Sucesso ao gravar o nome do carro no banco de dados");
+
+        } catch (Exception e){
+            System.out.println("Erro ao gravar o modelo do carro no banco de dados");
+        }
+    }
+}
+
--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,24 +1,37 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.model.Car;
+import br.com.carstore.dao.CarDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
-        String carName = request.getParameter("car-name");
+        String carName = request.getParameter("carName");
 
-        System.out.println(carName);
+        Car car = new Car();
 
-        request.getRequestDispatcher("index.html").forward(request, response);
+        car.setName(carName);
+
+        CarDAO carDao = new CarDAO();
+
+        carDao.createCar(car);
+
+
+
+        request.getRequestDispatcher("index.html").forward(request, resp);
 
     }
 
 }
+
